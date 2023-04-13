877fcbb8a163b0a1ab014486f027362196e46630
Vadim <counter97@mail.ru>
2023-04-13 19:31:17 +1000

    Adding project to git

--- src/test/java/hexlet/code/AppApplicationTests.java
+++ src/test/java/hexlet/code/AppApplicationTests.java
@@ -1,6 +1,5 @@
 package hexlet.code;
 
-import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
