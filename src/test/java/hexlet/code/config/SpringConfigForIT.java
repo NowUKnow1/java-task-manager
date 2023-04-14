8f72abd22b5ca9bec0b130a0c82b7466ae51c6d9
Vadim <counter97@mail.ru>
2023-04-15 02:11:40 +1000

    Changes

--- src/test/java/hexlet/code/config/SpringConfigForIT.java
+++ src/test/java/hexlet/code/config/SpringConfigForIT.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
 
-
 @Configuration
 @Profile(SpringConfigForIT.TEST_PROFILE)
 @EnableAspectJAutoProxy
